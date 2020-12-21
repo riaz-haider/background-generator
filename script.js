38b8d8bf1388f04b145f5b76698c3fcf7c0a9c78
riaz-haider <72768712+riaz-haider@users.noreply.github.com>
2020-12-20 22:40:30 -0600

    Adding starting project

---
+++ script.js
@@ -0,0 +1,15 @@
+var mycss = document.querySelector("h3");
+var mycolor1 = document.querySelector(".color1");
+var mycolor2 = document.querySelector(".color2");
+var mybody = document.getElementById("gradient");
+
+function setgradient() {
+
+    var newGradient = "linear-gradient(to right, " + mycolor1.value +  ", " +  mycolor2.value + ")";
+	mybody.style.background = newGradient;
+
+	mycss.textContent = mybody.style.background + ";";
+}
+
+mycolor1.addEventListener("input", setgradient);
+mycolor2.addEventListener("input", setgradient);
